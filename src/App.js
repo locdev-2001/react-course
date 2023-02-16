e18c1a7af36c2932aa7c52bbf28a7635659c4f12
vanvuong1118 <73210556+vanvuong1118@users.noreply.github.com>
2023-02-16 17:14:04 +0700

    16/2/2023

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
 // import logo from './logo.svg';
+import React,{useState} from "react";
 import "./App.css";
 import Expense from "./components/Expense/Expense";
 import NewExpense from "./components/NewExpense/NewExpense";
+const static_expense = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: "31231231",
+    title: "test",
+    amount: 45,
+    date: new Date(2022, 0, 26)
+  }
+];
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
-  const addExpenseHandler = expenses => {
-    console.log(expenses)
-    console.log('in appjs')
+  const [expenses, setExpenses] = useState(static_expense);
+  const addExpenseHandler = (expense) => {
+    console.log(expense)
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses,expense]
+    });
+    
+    // setExpenses([...expenses,expense]);
+    console.log(expenses,'in appjs')
   }
   return (
     <div>
-      <NewExpense addExpense={addExpenseHandler}/>
-      <Expense items={expenses}/>
+      <NewExpense addExpense={addExpenseHandler} />
+      <Expense items={expenses} />
     </div>
   );
 }
